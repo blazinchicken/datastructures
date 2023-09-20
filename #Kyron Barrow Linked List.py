815b0c99fd26e729add0c235255721b1e873aca7
blazinchicken <kyblazinchicken@gmail.com>
2023-09-20 04:06:00 -0500

    started the remove command and set baseline

--- #Kyron Barrow Linked List.py
+++ #Kyron Barrow Linked List.py
@@ -16,17 +16,18 @@ class LinkedList:
     ## initializing the head with None
         self.head = None
         self.n = 0
+        self.count = 0
 
-def display(self):
+    def display(self):
 ## variable for iteration
-    temp_node = self.head
+        temp_node = self.head
 ## iterating until we reach the end of the linked list
-    while temp_node != None:
+        while temp_node != None:
 ## printing the node data
-        print(temp_node.data, end='->')
+            print(temp_node.data, end='->')
 ## moving to the next node
-        temp_node = temp_node.next
-    print('Null')
+            temp_node = temp_node.next
+        print('Null')
 ##############################################
 ## Implement functions belows
 ##############################################
@@ -34,20 +35,59 @@ def display(self):
 # You can change the return values (from void to any) for each function as you want
 # you can add functions as you want
     def sortedAdd(self, value):
+        # make new node
+        new = Node(value)
+        # checks if node is empty clause
+        if self.head == None:
+            self.head = new
+            self.count += 1 
+            return
+        # still needs to add to the back if data < head data
+        # needs to set another variable to the head, then cycle through to the spot where the next value is higher than the current value, then insert into that spot. 
+        # to do this you need to set the current.next.next = new.next; current.next = new
+
+
         
     def remove(self, idx):
-
+        #check empty case
+        if self.head == None:
+            return
+        
+        current = self.head
+        #check to see if idx is within the bounds
+        if idx > self.count or idx <= 0:
+            return
+        #remove based on idx-1
+            #cycle through the list till the item before the idx that needs to be removed
+        for x in range(idx-1):
+            current = current.next
+        #set the new item to be linked
+            #check to see if this is removing the last of the list
+        if current.next.next == None:
+              current.next = None
+              return
+        
+        
 # find the maximum values in the list
     def findMax(self):
+        #count through the idx, this can be done by using the self.count 
+        #compare current.data & current.next.data
+        #set the higher one to be max_value, then try again, counting to self.count-1
+        #then return the found value
         return max_value in list
     
 # print linkedlist in a reversed order
     def printReversedList(self):
+        #make an array with self.count (self.count-1) pieces of data, count through the list, making current.data == array[i]
+        #this creates the list in order, then make another reverse array
+        #you set arrayTw0[i] = array[self.count-1-i] within a for loop
+        #then in the next for loop print the array
+
         if __name__ == '__main__':
 ## instantiating the linked list
             list = LinkedList()
 
-            
+
 # Your testcase will be here.
 # This is a testcase example.
 #list.sortedAdd(5)
